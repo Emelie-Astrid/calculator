903e2e7d52b6ede317f48e358c134c7b34cbaf56
Emelie <Emelie.almlof@gmail.com>
2023-02-08 21:28:38 +0100

    html, javascript, css files

---
+++ index.js
@@ -0,0 +1,57 @@
+const calculator = document.querySelector("#calculator");
+const input = document.querySelector("#input");
+
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const comma = document.querySelector("#comma");
+
+const divisonBtn = document.querySelector("#division");
+const multiplicationBtn = document.querySelector("#multiplication");
+const subsractionBtn = document.querySelector("#substraction");
+const additionBtn = document.querySelector("#addition");
+
+const result = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
+
+
+clear.addEventListener ("click", (e) => {
+    clearInput();
+})
+
+result.addEventListener ("click", () => {
+    displayResult();
+})
+
+function clearInput() {
+    input.value = "";
+}
+
+function displayResult() {
+    input.value = "Resultatet";
+}
+
+
+
+
+
+
+
+
+// additionBtn.addEventListener ("click", () => {
+
+//     let firstValue = document.querySelector("#input").value;
+//     let secondValue = document.querySelector("#input").value;
+
+//     let sum = firstValue + secondValue;
+//     console.log(sum);
+
+// })
+
